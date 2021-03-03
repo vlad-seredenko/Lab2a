81fee892b1ed57a4a80882c88867c6791d35a3c1
vlad-seredenko <78535638+vlad-seredenko@users.noreply.github.com>
2021-03-03 18:54:16 +0200

    Add files via upload

---
+++ Lab2a_lib/TownLib.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Lab2a_lib
+{
+    public class Town
+    {
+        public string Name;
+        public string Country;
+        public string Region;
+        public int Population;
+        public double YearIncome;
+        public double Square;
+        public bool HasPort;
+        public bool HasAirport;
+        public double yearIncomePerInhabitant
+        {
+            get
+            {
+                return GetYearIncomePerInhabitant();
+            }
+        }
+        public double GetYearIncomePerInhabitant()
+        {
+            return YearIncome / Population;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
